6774787981738c6041fe33f0c7ce2a8879841fd4
Josiah Seaman <josiah@newline.us>
2016-09-22 11:53:23 +0100

    #14 MAJOR: flipped ref_ and query_ variable names because I realized I had it backwards from the chain file definition

--- ChainParser.py
+++ ChainParser.py
@@ -44,7 +44,7 @@ def match(target, current):
 
 def fetch_all_chains(ref_chr, ref_strand, query_chr, query_strand, filename):
     """Fetches all chains that match the requirements.
-    any of {ref_chr, ref_strand, query_chr, query_strand} can be None which means the match to anything."""
+    any of {query_chr, query_strand, ref_chr, ref_strand} can be None which means the match to anything."""
     all_chains = []
     chain = []  # never actually used before assignment
     example_line = ''
@@ -66,7 +66,7 @@ def fetch_all_chains(ref_chr, ref_strand, query_chr, query_strand, filename):
                     if len(pieces) == 3 or len(pieces) == 1:
                         chain.append(line)  # "chain" will continue to be edited inside of "all_chains"
     if len(all_chains) == 0:
-        raise ValueError("A chain entry for %s and %s could not be found." % (ref_chr, query_chr) +
+        raise ValueError("A chain entry for %s and %s could not be found." % (query_chr, ref_chr) +
                          "     Example: %s" % example_line)
     return all_chains
 
@@ -87,28 +87,28 @@ def rev_comp(plus_strand):
 
 
 class ChainParser:
-    def __init__(self, chain_name, ref_source, query_source, output_folder_prefix, trial_run=False, swap_columns=False):
+    def __init__(self, chain_name, second_source, first_source, output_folder_prefix, trial_run=False, swap_columns=False):
         self.width_remaining = defaultdict(lambda: 70)
         self.chain_name = chain_name
-        self.ref_source = ref_source
-        self.query_source = query_source
+        self.ref_source = first_source  # example hg38ToPanTro4.chain  hg38 is the reference, PanTro4 is the query (has strand flips)
+        self.query_source = second_source
         self.output_folder_prefix = output_folder_prefix
         self.trial_run = trial_run
         self.swap_columns = swap_columns
-        self.ref_sequence = ''
         self.query_sequence = ''
-        self.ref_seq_gapped = array('u', '')
+        self.ref_sequence = ''
         self.query_seq_gapped = array('u', '')
+        self.ref_seq_gapped = array('u', '')
 
 
-    def read_seq_to_memory(self, ref_chr, query_chr, query_source, ref_source, query_file_name, ref_file_name):
-        self.query_sequence, contig_header = find_contig(query_chr, query_source)
-        if not self.trial_run:
-            self.write_fasta_lines(query_file_name, self.query_sequence)
-
+    def read_seq_to_memory(self, query_chr, ref_chr, ref_source, query_source, ref_file_name, query_file_name):
         self.ref_sequence, contig_header = find_contig(ref_chr, ref_source)
         if not self.trial_run:
             self.write_fasta_lines(ref_file_name, self.ref_sequence)
+
+        self.query_sequence, contig_header = find_contig(query_chr, query_source)
+        if not self.trial_run:
+            self.write_fasta_lines(query_file_name, self.query_sequence)
 
 
     def write_fasta_lines(self, filestream, seq):
@@ -116,7 +116,7 @@ class ChainParser:
             file_name = filestream
             with open(file_name, 'w') as filestream:
                 self.do_write(filestream, 70, seq)
-                print("Wrote", file_name, len(self.query_sequence))
+                print("Wrote", file_name, len(self.ref_sequence))
         else:
             remaining = self.width_remaining[filestream]
             self.do_write(filestream, remaining, seq)
@@ -139,46 +139,44 @@ class ChainParser:
 
     def mash_fasta_and_chain_together(self, chain_lines, is_master_alignment=False):
         header, chain_lines = chain_lines[0], chain_lines[1:]
-        query_pointer, ref_pointer = self.setup_chain_start(header, is_master_alignment)
+        ref_pointer, query_pointer = self.setup_chain_start(header, is_master_alignment)
 
         if not is_master_alignment:
-            self.do_translocation_housework(header, chain_lines, ref_pointer, query_pointer)
-        self.process_chain_body(chain_lines, ref_pointer, query_pointer, is_master_alignment)
+            self.do_translocation_housework(header, chain_lines, query_pointer, ref_pointer)
+        self.process_chain_body(chain_lines, query_pointer, ref_pointer, is_master_alignment)
         return True
 
 
-    def process_chain_body(self, chain_lines, ref_pointer, query_pointer, is_master_alignment):
+    def process_chain_body(self, chain_lines, query_pointer, ref_pointer, is_master_alignment):
         for chain_line in chain_lines:
             pieces = chain_line.split()
             if len(pieces) == 3:
-                size, gap_reference, gap_query = [int(x) for x in pieces]
-                if self.swap_columns:
-                    gap_reference, gap_query = gap_query, gap_reference
+                size, gap_query, gap_ref = [int(x) for x in pieces]
 
                 # Debugging code
-                if is_master_alignment and self.trial_run and len(self.query_seq_gapped) > 1000000:  # 9500000
+                if is_master_alignment and self.trial_run and len(self.ref_seq_gapped) > 1000000:  # 9500000
                     break
 
-                ref_seq_absolute = self.ref_sequence[ref_pointer: ref_pointer + size + gap_query]
-                ref_snippet = ref_seq_absolute + 'X' * gap_reference
-                ref_pointer += size + gap_query  # alignable and unalignable block concatenated together
-                self.ref_seq_gapped.extend(ref_snippet)
-
-                query_snippet = self.query_sequence[query_pointer: query_pointer + size] + 'X' * gap_query
-                query_snippet += self.query_sequence[query_pointer + size: query_pointer + size + gap_reference]
-                query_pointer += size + gap_reference  # two blocks of sequence separated by gap
+                query_seq_absolute = self.query_sequence[query_pointer: query_pointer + size + gap_ref]
+                query_snippet = query_seq_absolute + 'X' * gap_query
+                query_pointer += size + gap_ref  # alignable and unalignable block concatenated together
                 self.query_seq_gapped.extend(query_snippet)
 
-                if len(query_snippet) != len(ref_snippet):
-                    print(len(ref_snippet), len(query_snippet), "You should be outputting equal length strings til the end")
+                ref_snippet = self.ref_sequence[ref_pointer: ref_pointer + size] + 'X' * gap_ref
+                ref_snippet += self.ref_sequence[ref_pointer + size: ref_pointer + size + gap_query]
+                ref_pointer += size + gap_query  # two blocks of sequence separated by gap
+                self.ref_seq_gapped.extend(ref_snippet)
+
+                if len(ref_snippet) != len(query_snippet):
+                    print(len(query_snippet), len(ref_snippet), "You should be outputting equal length strings til the end")
 
             elif len(pieces) == 1:
                 # if is_master_alignment:  # last one: print out all remaining sequence
-                #     # ref_gapped_strand.extend(self.ref_sequence[ref_pointer:])
-                #     # self.query_seq_gapped.extend(self.query_sequence[query_pointer:])
+                #     # query_gapped_strand.extend(self.query_sequence[query_pointer:])
+                #     # self.ref_seq_gapped.extend(self.ref_sequence[ref_pointer:])
                 # else:
-                    self.query_seq_gapped.extend(self.query_sequence[query_pointer: query_pointer + int(pieces[0])])
                     self.ref_seq_gapped.extend(self.ref_sequence[ref_pointer: ref_pointer + int(pieces[0])])
+                    self.query_seq_gapped.extend(self.query_sequence[query_pointer: query_pointer + int(pieces[0])])
 
 
     def setup_chain_start(self, header, is_master_alignment):
@@ -186,24 +184,24 @@ class ChainParser:
         label, score, tName, tSize, tStrand, tStart, tEnd, qName, qSize, qStrand, qStart, qEnd, chain_id = header.split()
         # convert to int except for chr names and strands
         score, tSize, tStart, tEnd, qSize, qStart, qEnd, chain_id = [int(x) for x in (score, tSize, tStart, tEnd, qSize, qStart, qEnd, chain_id)]
-        query_pointer, ref_pointer = (qStart, tStart) if self.swap_columns else (tStart, qStart)   # this is correct, don't switch these around
+        ref_pointer, query_pointer = tStart, qStart
         if tEnd - tStart > 100 * 1000:
             print('>>>>', header)
         if is_master_alignment:  # include the unaligned beginning of the sequence
-            longer_gap = max(ref_pointer, query_pointer)
-            self.ref_seq_gapped.extend(self.ref_sequence[:ref_pointer] + 'X' * (longer_gap - ref_pointer))  # one of these gaps will be 0
-            self.query_seq_gapped.extend(self.query_sequence[:query_pointer] + 'X' * (longer_gap - query_pointer))
-        return query_pointer, ref_pointer
+            longer_gap = max(query_pointer, ref_pointer)
+            self.query_seq_gapped.extend(self.query_sequence[:query_pointer] + 'X' * (longer_gap - query_pointer))  # one of these gaps will be 0
+            self.ref_seq_gapped.extend(self.ref_sequence[:ref_pointer] + 'X' * (longer_gap - ref_pointer))
+        return ref_pointer, query_pointer
 
 
-    def do_translocation_housework(self, header, chain_lines, ref_pointer, query_pointer):
+    def do_translocation_housework(self, header, chain_lines, query_pointer, ref_pointer):
         minus_strand = header.split()[9] != '+'
         filler = 'G' if not minus_strand else 'C'
-        self.ref_seq_gapped.extend(filler * (500 + (100 - len(self.ref_seq_gapped) % 100)))  # visual separators
-        self.query_seq_gapped.extend(filler * (500 + (100 - len(self.query_seq_gapped) % 100)))
+        self.query_seq_gapped.extend(filler * (500 + (100 - len(self.query_seq_gapped) % 100)))  # visual separators
+        self.ref_seq_gapped.extend(filler * (500 + (100 - len(self.ref_seq_gapped) % 100)))
         # label, score, tName, tSize, tStrand, tStart, tEnd, qName, qSize, qStrand, qStart, qEnd, chain_id = header.split()
-        # self.ref_seq_gapped.extend('_'.join(['\n>' + tName, qStrand, qName]) + '\n')  # visual separators
-        # self.query_seq_gapped.extend('_'.join(['\n>' + qName, qStrand, tName]) + '\n')
+        # self.query_seq_gapped.extend('_'.join(['\n>' + tName, qStrand, qName]) + '\n')  # visual separators
+        # self.ref_seq_gapped.extend('_'.join(['\n>' + qName, qStrand, tName]) + '\n')
 
         # delete the ungapped query sequence
         # 	delete the query sequence that doesn't match to anything based on the original start, stop, size,
@@ -219,49 +217,49 @@ class ChainParser:
         # insert the gapped versions on both sides
         pass
 
-    def write_gapped_fasta(self, ref, query):
-        ref_gap_name = os.path.splitext(ref)[0] + '_gapped.fa'
+    def write_gapped_fasta(self, query, reference):
         query_gap_name = os.path.splitext(query)[0] + '_gapped.fa'
-        with open(ref_gap_name, 'w') as ref_file:
-            self.write_fasta_lines(ref_file, ''.join(self.ref_seq_gapped))
+        ref_gap_name = os.path.splitext(reference)[0] + '_gapped.fa'
         with open(query_gap_name, 'w') as query_file:
             self.write_fasta_lines(query_file, ''.join(self.query_seq_gapped))
-        return ref_gap_name, query_gap_name
+        with open(ref_gap_name, 'w') as ref_file:
+            self.write_fasta_lines(ref_file, ''.join(self.ref_seq_gapped))
+        return query_gap_name, ref_gap_name
 
 
-    def print_only_unique(self, ref_gapped_name, query_gapped_name):
-        ref_uniq_array = array('u', self.ref_seq_gapped)
-        que_uniq_array = array('u', self.query_seq_gapped)
+    def print_only_unique(self, query_gapped_name, ref_gapped_name):
+        query_uniq_array = array('u', self.query_seq_gapped)
+        que_uniq_array = array('u', self.ref_seq_gapped)
         print("Done allocating unique array")
-        shortest_sequence = min(len(self.ref_seq_gapped), len(self.query_seq_gapped))
+        shortest_sequence = min(len(self.query_seq_gapped), len(self.ref_seq_gapped))
         # scanning_past_header = False
         for i in range(shortest_sequence):
-            # if self.ref_seq_gapped[i] == '\n':
+            # if self.query_seq_gapped[i] == '\n':
             #     scanning_past_header = False  # stop scanning once you hit the terminating newline
             #     continue
-            # if self.ref_seq_gapped[i] in '>;':  # ; is for comments
+            # if self.query_seq_gapped[i] in '>;':  # ; is for comments
             #     scanning_past_header = True
-            # if scanning_past_header:  # ref_uniq_array is already initialized to contain header characters
+            # if scanning_past_header:  # query_uniq_array is already initialized to contain header characters
             #     continue
             # only overlapping section
-            if self.ref_seq_gapped[i] == self.query_seq_gapped[i]:
-                ref_uniq_array[i] = 'X'
+            if self.query_seq_gapped[i] == self.ref_seq_gapped[i]:
+                query_uniq_array[i] = 'X'
                 que_uniq_array[i] = 'X'
             else:  # Not equal
-                if self.ref_seq_gapped[i] == 'N':
-                    ref_uniq_array[i] = 'X'
                 if self.query_seq_gapped[i] == 'N':
+                    query_uniq_array[i] = 'X'
+                if self.ref_seq_gapped[i] == 'N':
                     que_uniq_array[i] = 'X'
 
         # Just to be thorough: prints aligned section (shortest_sequence) plus any dangling end sequence
-        ref_unique_name = ref_gapped_name[:-10] + '_unique.fa'
         query_unique_name = query_gapped_name[:-10] + '_unique.fa'
-        with open(ref_unique_name, 'w') as ref_filestream:
-            self.write_fasta_lines(ref_filestream, ''.join(ref_uniq_array))
+        ref_unique_name = ref_gapped_name[:-10] + '_unique.fa'
         with open(query_unique_name, 'w') as query_filestream:
-            self.write_fasta_lines(query_filestream, ''.join(que_uniq_array))
+            self.write_fasta_lines(query_filestream, ''.join(query_uniq_array))
+        with open(ref_unique_name, 'w') as ref_filestream:
+            self.write_fasta_lines(ref_filestream, ''.join(que_uniq_array))
 
-        return ref_unique_name, query_unique_name
+        return query_unique_name, ref_unique_name
 
 
     @staticmethod
@@ -280,55 +278,65 @@ class ChainParser:
 
         if isinstance(chromosome_name, str):
             chromosome_name = (chromosome_name, chromosome_name)
-        ref_chr, query_chr = chromosome_name
+        query_chr, ref_chr = chromosome_name
 
 
-        names = {'query': query_chr + '_%s.fa' % first_word(self.query_source),
-                 'ref': ref_chr + '_%s.fa' % first_word(self.ref_source)}  # for collecting all the files names in a modifiable way
+        names = {'ref': ref_chr + '_%s.fa' % first_word(self.ref_source),
+                 'query': query_chr + '_%s.fa' % first_word(self.query_source)}  # for collecting all the files names in a modifiable way
 
-        self.read_seq_to_memory(ref_chr, query_chr, self.query_source, self.ref_source, names['query'], names['ref'])
+        self.read_seq_to_memory(query_chr, ref_chr, self.ref_source, self.query_source, names['ref'], names['query'])
+
         # Reference chain that establishes coordinate frame
         all_chains = fetch_all_chains(ref_chr, '+', query_chr, '+', filename=self.chain_name)
-        master_chain, all_chains = all_chains[0], all_chains[1:]  # skip the reference chain
+        master_chain, translocations = all_chains[0], all_chains[1:]  # skip the reference chain
         self.mash_fasta_and_chain_together(master_chain, True)
-        for chain in all_chains:
+        for chain in translocations:
             self.mash_fasta_and_chain_together(chain, False)
+
         # All the inversions ###
         """In panTro4ToHg38.over.chain there are ZERO chains that have a negative strand on the reference 'tStrand'.
         I think it's a rule that you always flip the query strand instead."""
-        all_chains = fetch_all_chains(ref_chr, '+', query_chr, '-', filename=self.chain_name)
-        self.ref_sequence = rev_comp(self.ref_sequence)
-        for chain in all_chains:
+        inversions = fetch_all_chains(ref_chr, '+', query_chr, '-', filename=self.chain_name)
+        self.query_sequence = rev_comp(self.query_sequence)
+        for chain in inversions:
             self.mash_fasta_and_chain_together(chain, False)
 
-        names['ref_gapped'], names['query_gapped'] = self.write_gapped_fasta(names['ref'], names['query'])
-        names['ref_unique'], names['query_unique'] = self.print_only_unique(names['ref_gapped'], names['query_gapped'])
-        print("Finished creating gapped fasta files", names['query'], names['ref'])
+        names['query_gapped'], names['ref_gapped'] = self.write_gapped_fasta(names['query'], names['ref'])
+        names['query_unique'], names['ref_unique'] = self.print_only_unique(names['query_gapped'], names['ref_gapped'])
+        print("Finished creating gapped fasta files", names['ref'], names['query'])
 
-        folder_name = self.output_folder_prefix + query_chr
+        folder_name = self.output_folder_prefix + ref_chr
         source_path = '.\\bin\\Release\\output\\dnadata\\'
         if True:  #self.trial_run:  # these files are never used in the viz
-            del names['query']
             del names['ref']
+            del names['query']
         self.move_fasta_source_to_destination(names, folder_name, source_path)
-        DDV.DDV_main(['DDV',
-                      names['query_gapped'],
-                      source_path,
-                      folder_name,
-                      names['query_unique'], names['ref_unique'],
-                      names['ref_gapped']])
+        if self.swap_columns:
+            DDV.DDV_main(['DDV',
+                          names['query_gapped'],
+                          source_path, folder_name,
+                          names['query_unique'],
+                          names['ref_unique'],
+                          names['ref_gapped']])
+        else:
+            DDV.DDV_main(['DDV',
+                          names['ref_gapped'],
+                          source_path, folder_name,
+                          names['ref_unique'],
+                          names['query_unique'],
+                          names['query_gapped']])
 
 
 def do_chromosome(chr):
     parser = ChainParser(chain_name='hg38ToPanTro4.over.chain',
-                         ref_source='panTro4_chr20.fa',  # 'panTro4.fa',
-                         query_source='hg38_chr20.fa',  # 'HongKong\\hg38.fa',
-                         output_folder_prefix='panTro4_and_Hg38_alignment2_',
-                         trial_run=False,
-                         swap_columns=False)
+                         first_source='hg38_chr20.fa',  # 'HongKong\\hg38.fa',
+                         second_source='panTro4_chr20.fa',  # 'panTro4.fa',
+                         output_folder_prefix='panTro4_and_Hg38_alignment0_',
+                         trial_run=True,
+                         swap_columns=True)
     # parser = ChainParser(chain_name='HongKong\\human_gorilla.bland.chain',
-    #                      query_source='HongKong\\susie3_agp.fasta',
-    #                      ref_source='HongKong\\hg38.fa',
+    #                      ref_source='HongKong\\susie3_agp.fasta',
+    #                      query_source='HongKong\\hg38.fa',
     #                      output_folder_prefix='Susie3_and_Hg38_short3_',
     #                      trial_run=True,
     #                      swap_columns=True)
