ad2502a214c7038d77bc5f474991218168f61377
Josiah Seaman <josiah@newline.us>
2016-10-28 14:48:54 +0100

    #21 added purge_annotation feature to cut down on duplicates

--- Annotations.py
+++ Annotations.py
@@ -46,8 +46,10 @@ class GFF(object):
 
                 if chromosome not in annotations:
                     annotations[chromosome] = []
-                    if len(annotations) < 40:
-                        print(chromosome)
+                    if len(annotations) < 10:
+                        print(chromosome, end=", ")
+                    elif len(annotations) == 10:
+                        print('...')
 
                 ID = counter
                 source = elements[1]
@@ -79,7 +81,7 @@ class GFF(object):
                                              source, feature,
                                              start, end,
                                              score, strand,
-                                             frame, attribute)
+                                             frame, attribute, line)
 
                 annotations[chromosome].append(annotation)
 
@@ -91,7 +93,7 @@ class GFF(object):
         return specimen, gff_version, genome_version, date, file_name, annotations
 
     class Annotation(object):
-        def __init__(self, chromosome, ID, source, feature, start, end, score, strand, frame, attribute):
+        def __init__(self, chromosome, ID, source, feature, start, end, score, strand, frame, attribute, line):
             assert isinstance(chromosome, str)
             assert isinstance(ID, int)
             assert isinstance(source, str)
@@ -113,6 +115,7 @@ class GFF(object):
             self.strand = strand
             self.frame = frame
             self.attribute = attribute
+            self.line = line
 
 
 def create_fasta_from_annotation(gff_filename, target_chromosome, out_name, chromosome_length):
@@ -135,11 +138,42 @@ def create_fasta_from_annotation(gff_filename, target_chromosome, out_name, chro
                         if seq[i] == filler:
                             seq[i] = 'T'
 
-    write_complete_fasta(out_name, seq)
+    write_complete_fasta(out_name, seq, header='>%s\n' % target_chromosome)
     print("Done", gff.file_name)
     print("Found %i exons" % count)
 
 
+def purge_annotation(gff_filename):
+    features_of_interest = ['exon', 'gene']
+    gff = GFF(gff_filename)
+    total = 0
+    kept = 0
+    survivors = []
+    for chromosome in gff.annotations.keys():
+        for entry in gff.annotations[chromosome]:
+            assert isinstance(entry, GFF.Annotation), "I'm confused"
+            total += 1
+            if entry.feature in features_of_interest:
+                if survivors:
+                    last = survivors[-1]
+                    if last.start == entry.start and last.end == entry.end and entry.feature == last.feature:
+                        continue  # skip this because it's a duplicate of what we already have
+                kept += 1
+                survivors.append(entry)
+
+    with open(gff_filename[:-4] + '_trimmed.gtf', 'w') as out:
+        for entry in survivors:
+            out.write(entry.line)
+
+    print("Done", gff.file_name)
+    print("Kept %.2f percent = %i / %i" % (kept / total * 100, kept, total))
+
+
 if __name__ == '__main__':
-    target_chromosome = r'HongKong\Pan_Troglodytes_refseq2.1.4.gtf'
-    create_fasta_from_annotation(target_chromosome, 'chr20', 'Chimp_test_' + target_chromosome + '.fa', 63 * 1000 * 1000)
+    # annotation = r'HongKong\Pan_Troglodytes_refseq2.1.4.gtf'
+    # target_chromosome = 'chr20'
+    # create_fasta_from_annotation(annotation, target_chromosome, 'Chimp_test_' + target_chromosome + '.fa', 63 * 1000 * 1000)
+
+    # annotation = r'HongKong\Pan_Troglodytes_refseq2.1.4.gtf'
+    annotation = r'HongKong\Homo_Sapiens_GRCH38_trimmed.gtf'
+    purge_annotation(annotation)
