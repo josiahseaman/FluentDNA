57d45c6a8b3a72524ffeb38b71b59fe3b8904f54
Josiah Seaman <josiah@newline.us>
2018-01-10 10:33:57 +0000

    Swapped colors for A and T to be consistent with --high_contrast colors

--- DDV/TileLayout.py
+++ DDV/TileLayout.py
@@ -70,8 +70,8 @@ class TileLayout(object):
         self.palette['Y'] = hex_to_rgb('4B4BB5')
 
         #-----Nucleotide Colors! Paletton Quadrapole colors------
-        self.palette['T'] = hex_to_rgb('C35653')  # Red
-        self.palette['A'] = hex_to_rgb('D4A16A')  # Orange
+        self.palette['A'] = hex_to_rgb('C35653')  # Red
+        self.palette['T'] = hex_to_rgb('D4A16A')  # Orange
         self.palette['G'] = hex_to_rgb('55AA55')  # Green
         self.palette['C'] = hex_to_rgb('457585')  # Blue
         if self.high_contrast:
